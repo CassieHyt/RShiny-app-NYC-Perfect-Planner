0daa66812389a5429fa17599de3ece1b543aa922
ZixuanGuan <zg2244@columbia.edu>
2017-02-19 17:00:15 -0500

    Latest

--- lib/server.R
+++ lib/server.R
@@ -1,107 +1,148 @@
 library(leaflet)
 library(shiny)
 
+Theater<-read.csv("Theater.csv",header=T)
+Library<-read.csv("Library.csv",header=T)
+Deli<-read.csv("Deli.csv",header=T)
+Museum<-read.csv("Museum.csv",header=T)
+Restaurant<-read.csv("Restaurant.csv",header=T)
+Gallery<-read.csv("Gallery.csv",header=T)
+Market<-read.csv("Market.csv",header=T)
+
 
 
 shinyServer(function(input,output){
-  
-  map=leaflet()%>%
-    addProviderTiles("Hydda.Full")%>%
-    setView(lng = -73.97, lat = 40.70, zoom = 30)%>%
-    addMarkers(data=Deli,
-              group="Deli",
-               clusterOptions = markerClusterOptions(),
-               lng=~LON,lat=~LAT,
-               icon=~myIcons$Deli,
-               popup=paste("Name:",Deli$NAME,"<br/>",
-                           "Tel:",Deli$TEL,"<br/>",
-                           "Website:",Deli$URL,"<br/>",
-                           "Address:",Deli$ADDRESS))%>%
-    addMarkers(data=Theater,
-               group="Theater",
-               clusterOptions = markerClusterOptions(),
-               lng=~LON,lat=~LAT,
-               icon=~myIcons$Theater,
-               popup=paste("Name:",Theater$NAME,"<br/>",
-                           "Tel:",Theater$TEL,"<br/>",
-                           "Website:",Theater$URL,"<br/>",
-                           "Address:",Theater$ADDRESS))%>%
-    addMarkers(data=Library,
-               group="Library",
-               clusterOptions = markerClusterOptions(),
-               lng=~LON,lat=~LAT,
-               icon=~myIcons$Library,
-               popup=paste("Name:",Library$NAME,"<br/>",
-                           "Tel:",Library$TEL,"<br/>",
-                           "Website:",Library$URL,"<br/>",
-                           "Address:",Library$ADDRESS))%>%
-    addMarkers(data=Museum,
-               group="Museum",
-               clusterOptions = markerClusterOptions(),
-               lng=~LON,lat=~LAT,
-               icon=~myIcons$Museum,
-               popup=paste("Name:",Museum$NAME,"<br/>",
-                           "Tel:",Museum$TEL,"<br/>",
-                           "Website:",Museum$URL,"<br/>",
-                           "Address:",Museum$ADDRESS))%>%
+  output$map <- renderLeaflet({
+    map <- leaflet() %>%  addTiles(
+      urlTemplate = "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhcG9sZW9uIiwiYSI6ImNpa3Q0cXB5bTAwMXh2Zm0zczY1YTNkd2IifQ.rjnjTyXhXymaeYG6r2pclQ",
+      attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>' )  %>% setView(-73.983,40.7639,zoom = 13)
     
-    addMarkers(data=Gallery,
-               group="Gallery",
-               clusterOptions = markerClusterOptions(),
-               lng=~LON,lat=~LAT,
-               icon=~myIcons$Gallery,
-               popup=paste("Name:",Gallery$NAME,"<br/>",
-                           "Tel:",Gallery$TEL,"<br/>",
-                           "Website:",Gallery$URL,"<br/>",
-                           "Address:",Gallery$ADDRESS))%>%
-    addMarkers(data=Restaurant,
-               group="Restaurant",
-               clusterOptions = markerClusterOptions(),
-               lng=~LON,lat=~LAT,
-               icon=~myIcons$Restaurant,
-               popup=paste("Name:",Restaurant$NAME,"<br/>",
-                           "Tel:",Restaurant$TEL,"<br/>",
-                           "Website:",Restaurant$URL,"<br/>",
-                           "Address:",Restaurant$ADDRESS))%>%
-  
-    hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library"))
-  
-  output$map=renderLeaflet(map)
-  
-  
- observeEvent(input$choice1,{
-   leafletProxy("map") %>%
-     hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library"))%>%
-     showGroup(input$choice1)
+    leafletProxy("map",data=Theater) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Theater$LON,lat=Theater$LAT,
+                 icon=list(iconUrl='icon/Theater.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Theater$NAME,"<br/>",
+                             "Tel:",Theater$TEL,"<br/>",
+                             "Zipcode:",Theater$ZIP,"<br/>",
+                             "Website:",a(Theater$URL, href = Theater$URL),"<br/>", # warning appears
+                             "Address:",Theater$ADDRESS),
+                 group="Theater" )
     
- })    
- 
- observeEvent(input$choice2,{
-   leafletProxy("map")%>%
-     hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library"))%>%
-     showGroup(c(input$choice1,input$choice2))
- })
-  
- 
- 
- observeEvent(input$choice3,{
-   leafletProxy("map")%>%
-     hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library"))%>%
-     showGroup(c(input$choice1,input$choice2,input$choice3))
- })
- 
-  
- observeEvent(input$update,{
-   
-   output$c1<-renderText("abcsdg")
- })
- 
-  observeEvent(input$zoom, {
-    leafletProxy('map') %>%
-      setView(map,lat=data$LAT,lng=data$LON,zoom=520)
+    leafletProxy("map",data=Library) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Library$LON,lat=Library$LAT,
+                 icon=list(iconUrl='icon/Library.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Library$NAME,"<br/>",
+                             "Tel:",Library$TEL,"<br/>",
+                             "Zipcode:",Library$ZIP,"<br/>",
+                             "Website:",a(Library$URL, href = Library$URL),"<br/>", # warning appears
+                             "Address:",Library$ADDRESS),
+                 group="Library" )
+    
+    leafletProxy("map",data=Deli) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Deli$LON,lat=Deli$LAT,
+                 icon=list(iconUrl='icon/Deli.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Deli$NAME,"<br/>",
+                             "Tel:",Deli$TEL,"<br/>",
+                             "Zipcode:",Deli$ZIP,"<br/>",
+                             "Website:",a(Deli$URL, href = Deli$URL),"<br/>", # warning appears
+                             "Address:",Deli$ADDRESS),
+                 group="Deli" )
+    
+    leafletProxy("map",data=Museum) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Museum$LON,lat=Museum$LAT,
+                 icon=list(iconUrl='icon/Museum.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Museum$NAME,"<br/>",
+                             "Tel:",Museum$TEL,"<br/>",
+                             "Zipcode:",Museum$ZIP,"<br/>",
+                             "Website:",a(Museum$URL, href = Museum$URL),"<br/>", # warning appears
+                             "Address:",Museum$ADDRESS),
+                 group="Museum" )
+    
+    leafletProxy("map",data=Restaurant) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Restaurant$LON,lat=Restaurant$LAT,
+                 icon=list(iconUrl='icon/Restaurant.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Restaurant$NAME,"<br/>",
+                             "District:",Restaurant$DISTRICT,"<br/>",
+                             "Type:",Restaurant$TYPE,"<br/>",
+                             "Address:",Restaurant$ADDRESS),
+                 group="Restaurant" )
+    
+    
+    leafletProxy("map",data=Gallery) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Gallery$LON,lat=Gallery$LAT,
+                 icon=list(iconUrl='icon/Gallery.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Gallery$NAME,"<br/>",
+                             "Tel:",Gallery$TEL,"<br/>",
+                             "Zipcode:",Gallery$ZIP,"<br/>",
+                             "Website:",a(Gallery$URL, href = Gallery$URL),"<br/>", # warning appears
+                             "Address:",Gallery$ADDRESS),
+                 group="Gallery" )
+    
+    leafletProxy("map",data=Market) %>%
+      addMarkers(clusterOptions = markerClusterOptions(),
+                 lng=Market$LON,lat=Market$LAT,
+                 icon=list(iconUrl='icon/Market.png',iconSize=c(20,20)),
+                 popup=paste("Name:",Market$NAME,"<br/>",
+                             "Tel:",Market$TEL,"<br/>",
+                             "Zipcode:",Market$ZIP,"<br/>",
+                             "Website:",a(Market$URL, href = Market$URL),"<br/>", # warning appears
+                             "Address:",Market$ADDRESS),
+                 group="Market" )
+    
+    
+    map%>%hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library","Market"))
   })
   
   
+  observeEvent(input$submit11,{
+    url = paste0('http://maps.google.com/maps/api/geocode/xml?address=',input$location,'&sensor=false')
+    doc = xmlTreeParse(url) 
+    root = xmlRoot(doc) 
+    lat = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lat']])) 
+    long = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lng']]))
+    
+    leafletProxy("map") %>%
+      clearMarkers() %>%
+      #      addMarkers(data=address(),~longitude,lat=latitude)
+      addMarkers(lng=long,lat=lat)
+  })
+  
+  observeEvent(input$choice1,{
+    leafletProxy("map") %>%
+      showGroup(input$choice1)
+    
+  })    
+  observeEvent(input$choice1,{
+    if("NA"==input$choice1){leafletProxy("map")%>%hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library","Market"))}
+  })
+  
+  observeEvent(input$choice2,{
+    leafletProxy("map")%>%
+        showGroup(c(input$choice1,input$choice2))
+  })
+  
+  
+  
+  observeEvent(input$choice3,{
+    if("NA" == input$choice3){leafletProxy("map")%>%showGroup(c(input$choice1,input$choice2))}
+    else {leafletProxy("map")%>%
+        hideGroup(c("Deli","Museum","Theater","Gallery","Restaurant","Library","Market"))%>%
+        showGroup(c(input$choice1,input$choice2,input$choice3))}
+  })
+  
+  
+  observeEvent(input$submit, {
+    leafletProxy('map') %>%
+      addCircles(lng =Theater$LON,lat=Theater$LAT,radius = 1, group='circles')})
+  
+  observeEvent(input$clear, {
+    leafletProxy('map') %>% clearGroup("circles")
+  })
   
 })
 
