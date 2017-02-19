1312a215bdfb3ae8d8d529672bd2cf73827ac4b1
ZixuanGuan <zg2244@columbia.edu>
2017-02-19 11:12:06 -0500

    mark problem

--- lib/server copy.R
+++ lib/server copy.R
@@ -8,7 +8,9 @@ Library<-read.csv("Library.csv",header=T)
 
 shinyServer(function(input,output){
   output$map <- renderLeaflet({
-    map <- leaflet() %>%  addProviderTiles("Hydda.Full") %>% setView(-73.983,40.7639,zoom = 13)
+    map <- leaflet() %>%  addTiles(
+      urlTemplate = "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhcG9sZW9uIiwiYSI6ImNpa3Q0cXB5bTAwMXh2Zm0zczY1YTNkd2IifQ.rjnjTyXhXymaeYG6r2pclQ",
+      attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>' )  %>% setView(-73.983,40.7639,zoom = 13)
     
     leafletProxy("map",data=Theater[1:20,]) %>%
       addMarkers(clusterOptions = markerClusterOptions(),
@@ -35,6 +37,19 @@ shinyServer(function(input,output){
   })
   
 
+  observeEvent(input$location,{
+    url = paste0('http://maps.google.com/maps/api/geocode/xml?address=',input$location,'&sensor=false')
+    doc = xmlTreeParse(url) 
+    root = xmlRoot(doc) 
+    lat = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lat']])) 
+    long = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lng']]))
+    
+    leafletProxy("map") %>%
+      clearMarkers() %>%
+      #      addMarkers(data=address(),~longitude,lat=latitude)
+      addMarkers(lng=long,lat=lat)
+  })
+  
   observeEvent(input$submit, {
     if("Theater" %in% c(input$choice1,input$choice2,input$choice3) ) leafletProxy("map") %>% showGroup("markers_Theater")
     else{leafletProxy("map") %>% hideGroup("markers_Theater")}
--- lib/ui copy.R
+++ lib/ui copy.R
@@ -1,31 +1,53 @@
 
-library(leaflet)
+
 library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(shinythemes)
+library(XML)
+
 
 Selection<-c("Deli","Supermarket","Library","Theater","Restaurant","Museum")
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  titlePanel("Choose Your Preference"),
-  
-  sidebarLayout(
-    
-    sidebarPanel(
-      fluidRow(
-        textInput("My_location", label = h3("Enter Your Location"), value = "Enter text..."),
-        selectInput('choice1', 'Chioce 1:',Selection),
-        selectInput('choice2', 'Chioce 2:',Selection),
-        selectInput('choice3', 'Chioce 3:',Selection),
-        sliderInput("radius", "Radius", min = 0, max = 10, value = 0.1, step= 0.1),
-        actionButton("submit", "Submit"),
-        actionButton("clear","Clear")
-      )
-    ),
-    
-    mainPanel(
-      tabsetPanel(leafletOutput('map'))
-    )
-  )
-  
+
+shinyUI(navbarPage(theme = shinytheme("flatly"),
+                   
+                   "Trees in New York",id="nav",
+                   
+                   tabPanel("New York",
+                            div(class="outer",
+                                tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+                                leafletOutput("map",width="100%",height = "100%"),
+                                absolutePanel(id="control1",fixed = T,draggable = T,class = "panel panel-default",top = 80,
+                                              left = 55, right = "auto", bottom = "auto",
+                                              width = 280, height = 550,
+                                              h3("Trees"),
+                                              
+                                              helpText("Choose Your Location"),
+                                              textInput("location","Your Location:","Columbia University NY"),
+                                              actionButton("location","Mark"),style="opacity: 0.5",
+                                              textOutput("lo"),
+                                              selectInput('choice1', 'Chioce 1:',Selection),
+                                              selectInput('choice2', 'Chioce 2:',Selection),
+                                              selectInput('choice3', 'Chioce 3:',Selection),
+                                              sliderInput("radius", "Radius", min = 0, max = 10, value = 0.1, step= 0.1),
+                                              actionButton("submit", "Submit"),
+                                              actionButton("clear","Clear")
+                                              
+                                              
+                                )
+                                
+                                
+                                )
+                   ),
+                   
+                   tabPanel("New York"
+                   ),
+                   tabPanel("New York"
+                   )
+                   
+                   
 ))
+
+
+
