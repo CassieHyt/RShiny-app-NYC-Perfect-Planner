25f9a1736c942f77e642ce24498a4b147632912d
Jihan Wei <jw3447@columbia.edu>
2017-02-22 13:13:33 -0500

    2.22（final）

--- lib/global.R
+++ lib/global.R
@@ -1,16 +1,31 @@
 library(leaflet)
+library(geosphere)
+library(ggmap)
+require(RJSONIO)
+library(shiny)
+library(corrgram)
+library(plyr)
+library(ggplot2)
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(shinythemes)
+library(XML)
 
-setwd("C:/Users/sony/Desktop/ADS/Spr2017-proj2-grp14/data")
+Deli<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Deli.csv",header=T))
+Deli$URL<-"Unavailable"
+Market<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Market.csv",header=T))
+Market$URL<-"Unavailable"
+Gallery<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Gallery.csv",header=T))
+Library<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Library.csv",header=T))
+Museum<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Museum.csv",header=T))
+Restaurant<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Restaurant.csv",header=T))
+Restaurant$URL<-"Unavailable"
+Theater<-na.omit(read.csv("C:/Users/Jihan Wei/Desktop/data/Theater.csv",header=T))
+all_data<-list(Deli=Deli,Market=Market,Gallery=Gallery,Library=Library,Museum=Museum,Restaurant=Restaurant,Theater=Theater)
 
-Deli<-na.omit(read.csv("../data/Deli.csv",header=T))
-Market<-na.omit(read.csv("../data/Market.csv",header=T))
-Gallery<-na.omit(read.csv("../data/Gallery.csv",header=T))
-Library<-na.omit(read.csv("../data/Library.csv",header=T))
-Museum<-na.omit(read.csv("../data/Museum.csv",header=T))
-Restaurant<-na.omit(read.csv("../data/NCY resterant.csv",header=T))
-Theater<-na.omit(read.csv("../data/Theater.csv",header=T))
-data<-list(Deli=Deli,Market=Market,Gallery=Gallery,Library=Library,Museum=Museum,Restaurant=Restaurant,Theater=Theater)
-
+Type<-as.character(unique(Restaurant$TYPE))
+Selection=list(N.A="N.A",Market="Market",Deli="Deli",Library="Library",Theater="Theater",Musemu="Musemu",Gallery="Gallery",Restaurant=Type)
 
 myIcons <- iconList(
   Museum = makeIcon("museum.png", 18, 18),
@@ -24,86 +39,128 @@ myIcons <- iconList(
 
 
 
-#Load Testing
+geocodeAdddress <- function(address) {
+  url <- "http://maps.google.com/maps/api/geocode/json?address="
+  url <- URLencode(paste(url, address, "&sensor=false", sep = ""))
+  x <- fromJSON(url, simplify = FALSE)
+  if (x$status == "OK") {
+    out <- c(x$results[[1]]$geometry$location$lng,x$results[[1]]$geometry$location$lat)
+  } else {
+    out <- NA
+  }
+  Sys.sleep(0.2)  # API only allows 5 requests per second
+  out
+}
 
 
-
-
-Selection<-c("Deli","Supermarket","Library","Theater","Restaurant","Museum") 
-
+##Get Index for candidates
 get_Ind<-function(data,Lon0,Lat0,r){
-  dis<-(data$LON-Lon0)^2+(data$LAT-Lat0)^2
-  Ind<-dis<r^2
+  coords<-cbind(data$LON,data$LAT)
+  dis<-distm(coords,c(Lon0,Lat0), fun = distHaversine)
+  Ind<-dis<r
   return(list(dis=dis,Ind=Ind))
 }
 
+##Get data:
 get_candidate<-function(data,Lon0,Lat0,r){
-  Ind<-(data$LON-Lon0)^2+(data$LAT-Lat0)^2<r^2
+  coords<-cbind(data$LON,data$LAT)
+  dis<-distm(coords,c(Lon0,Lat0), fun = distHaversine)
+  Ind<-dis<r
   return(data[Ind,])
 }
 
-all_data<-list(Deli=Deli,Supermarket=Market)
 
 
+##########Give centers based on the selection
+########Output is dataframe. if no data satisfying the conditions, error messages will appear
 get_center<-function(choice1,choice2,choice3,Lon0,Lat0,distance,radius){
   
-  ##Get candidates accoriding to distance
-  data_candidate<-all_data[c(choice1,choice2,choice3)]
-  data_select<-lapply(data_candidate,get_candidate,Lon0=Lon0,Lat0=Lat0,r=distance)
+  if (choice1 %in% Type){ choice1="Restaurant"}
+  if (choice2 %in% Type){ choice2="Restaurant"}
+  if (choice3 %in% Type){ choice3="Restaurant"}
   
-  ##Get candidates according to the radius:
-  D1<-data_select[[choice1]]
-  D2<-data_select[[choice2]]
-  D3<-data_select[[choice3]]
-  I<-nrow(D1)
-  Ans1<-NULL
-  #Ans2<-NULL
-  #Ans3<-NULL
-  #r<-NULL
-  IN1<-NULL
-  IN2<-NULL
-  
-  ##Check
+  distance=distance*1000
+  radius=radius*1000
+  ##Check how many choices are made
   null2<-is.na(choice2)
   null3<-is.na(choice3)
   
   ##If the user only select "Choice1" 
   if (null2 & null3){
-    output_data<-D1
+    data_candidate<-all_data[[choice1]]
+    ##Get candidates according to the radius:
+    output_data<-get_candidate(data_candidate,Lon0=Lon0,Lat0=Lat0,r=distance)
+    if ((dim(output_data)[1]==0)){
+      mess<-"Sorry,No place found! Please try Longer Distance"
+      return(mess)
+    } else{
+      return(output_data)
+    }
   }
   
-  
-  ##If the user only select "Choice1" and "Choice2"
+  ##If the user only select "Choice1" and "Choice 2"
   if (!null2 & null3){
-    for(i in 1:I){
-      with2<-get_Ind(D2,D1[i,"LON"],D1[i,"LAT"],r=radius)
-      IN2<-with2$Ind
-      if (sum(IN2,na.rm=T)!=0){
-        Ans1<-c(Ans1,i)
-        #Ans2<-rbind(Ans2,IN2)
-        #Ans3<-rbind(Ans2,IN2)
+    data_candidate<-all_data[c(choice1,choice2)]
+    data_candidate<-na.omit(data_candidate)
+    ##Get candidates according to the radius:
+    data_select<-lapply(data_candidate,get_candidate,Lon0=Lon0,Lat0=Lat0,r=distance)
+    D1<-na.omit(data_select[[choice1]])
+    D2<-na.omit(data_select[[choice2]])
+    
+    if ((dim(D1)[1]==0)|((dim(D2)[1]==0))){
+      mess<-"Sorry,No place found! Please try Longer Distance"
+      return(mess)
+    } else {
+      coords1<-D1[,c("LON","LAT")]
+      coords2<-D2[,c("LON","LAT")]
+      ##Culculate distance between choices and selected the centers
+      Dis2<-distm(coords1,coords2, fun = distHaversine)
+      Inx2<-Dis2<radius
+      output_data<-D1[!(colSums(Inx2)==0),]
+      
+      if(dim(output_data)[1]==0){
+        mess<-"Sorry,No place found! Please try Longer Radius"
+        return(mess)
+      } else{
+        return(output_data)
       }
     }
-    output_data<-D1[Ans1,]
-  } 
-  if(!null2 & !null3){
-    for(i in 1:I){
-      with2<-get_Ind(D2,D1[i,"LON"],D1[i,"LAT"],r=radius)
-      with3<-get_Ind(D3,D1[i,"LON"],D1[i,"LAT"],r=radius)
-      IN2<-with2$Ind
-      IN3<-with3$Ind
-      if (sum(IN2,na.rm=T)!=0 & sum(IN3,na.rm=T)!=0){
-        Ans1<-c(Ans1,i)
-        #Ans2<-rbind(Ans2,IN2)
-        #Ans3<-rbind(Ans2,IN2)
-      }
-    }
-    output_data<-D1[Ans1,]
   }
-  return(output_data) 
+  
+  ##If the user selecte all choices
+  if (!null2 & !null3){
+    data_candidate<-all_data[c(choice1,choice2,choice3)]
+    
+    ##Get candidates according to the radius:
+    data_select<-lapply(data_candidate,get_candidate,Lon0=Lon0,Lat0=Lat0,r=distance)
+    D1<-na.omit(data_select[[choice1]])
+    D2<-na.omit(data_select[[choice2]])
+    D3<-na.omit(data_select[[choice3]])
+    
+    if ((dim(D1)[1]==0)|((dim(D2)[1]==0))|((dim(D3)[1]==0))){
+      mess<-"Sorry,No place found! Please try Longer Distance"
+      return(mess)
+    } else {
+      coords1<-D1[,c("LON","LAT")]
+      coords2<-D2[,c("LON","LAT")]
+      coords3<-D3[,c("LON","LAT")]
+      
+      ##Culculate distance between choices
+      Dis2<-distm(coords1,coords2, fun = distHaversine)
+      Dis3<-distm(coords1,coords3, fun = distHaversine)
+      Inx2<-Dis2<radius
+      Inx3<-Dis3<radius
+      output_data<-D1[(!(colSums(Inx2)==0))&(!(colSums(Inx3)==0)),]
+      
+      if(dim(output_data)[1]==0){
+        mess<-"Sorry,No place found! Please try Longer Radius"
+        return(mess)
+      } else{
+        return(output_data)
+      }
+    }
+  }
 }
-
-
 
 ##Testing
 # data<-get_center("Deli","Supermarket",NA,-73.68,40.67,0.1,0.01)
